b7d739791019e24c69e7d05a7bd957a59a148df7
Joseph Song <ijooson@gmail.com>
2022-04-28 01:12:11 -0500

    Adding search button

--- poke-react/src/App.jsx
+++ poke-react/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
 
 function useDocumentTitle(title) {
   useEffect(() => {
-    document.title = "Saying hello to " + title;
+    document.title = 'Saying hello to ' + title;
   }, [title]);
 }
 
 function Pokemon() {
-  let [pokemon, setPokemon] = useState("pikachu");
+  let [pokemon, setPokemon] = useState('pikachu');
   let [img, setImg] = useState(null);
   let [weaknesses, setWeaknesses] = useState(null);
   let [resistances, setResistances] = useState(null);
+
+  let inputRef = useRef();
 
   useDocumentTitle(pokemon);
 
@@ -39,47 +41,38 @@ function Pokemon() {
   return (
     <>
       <div className="top">
-        <input
-          type="text"
-          placeholder="Enter a Pokémon"
-          onChange={(e) => {
-            setPokemon(e.target.value);
-          }}
-        />
+        <div className="inputs">
+          <input type="text" placeholder="Enter a Pokémon" ref={inputRef} />
+          <button
+            onClick={() => {
+              setPokemon(inputRef.current.value);
+            }}
+          >
+            🔎
+          </button>
+        </div>
       </div>
       <div className="flex">
         <div className="list">
           {weaknesses && `Weaknesses`}
-          <ul>
-            {weaknesses && weaknesses.map((w) => (
-              <li key={w}>{w}</li>
-            ))
-          }
-          </ul>
+          <ul>{weaknesses && weaknesses.map((w) => <li key={w}>{w}</li>)}</ul>
         </div>
-        <div>
-          {img && <img src={img} alt={`Sprite for ${pokemon}`} />}
-        </div>
+        <div>{img && <img src={img} alt={`Sprite for ${pokemon}`} />}</div>
         <div className="list">
           {resistances && `Resistances`}
-          <ul>
-            {resistances && resistances.map((r) => (
-              <li key={r}>{r}</li>
-            ))
-          }
-          </ul>
+          <ul>{resistances && resistances.map((r) => <li key={r}>{r}</li>)}</ul>
         </div>
       </div>
     </>
   );
-};
+}
 
 function App() {
   return (
     <div className="pokemon">
       <Pokemon />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
